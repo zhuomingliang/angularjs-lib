7401b13aaf1b8c0985cf7ec3fcaddfadbaa98f3a
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2012-09-10 20:31:55 +0800

    add type directive

---
+++ directive/type/type.js
@@ -0,0 +1,16 @@
+angular.module('lib', []).
+  directive('type', function() {
+    return {
+      restrict: 'A', // only activate on element attribute
+      require: '?ngModel', // get a hold of NgModelController
+      link: function(scope, element, attrs, ngModel) {
+        if(attrs.type != 'phone') return;
+        if(!ngModel) return; // do nothing if no ng-model
+        scope.$watch(attrs.ngModel, function(value) {
+          if(value == undefined) ngModel.$setValidity('wrong', true);
+          else if(/^1\d{10}$/.test(value)) ngModel.$setValidity('wrong', true);
+          else ngModel.$setValidity('wrong', false);
+      });
+    }
+  };
+});
